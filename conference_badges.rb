f57b4cbea0db101bae38dfb96d3be4ff2e7a121e
georgetalnagi <gtalnagi@yahoo.com>
2020-10-22 18:02:35 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,6 @@
-
 def badge_maker(name)
     "Hello, my name is #{name}."
 end 
-
 
 def batch_badge_creator(names)
     badges = []
@@ -10,7 +8,6 @@ def batch_badge_creator(names)
     badges 
 end 
 
- 
 def assign_rooms(names)
     rooms = []
     names.each.with_index(1){|name, room| rooms << "Hello, #{name}! You'll be assigned to room #{room}!"}
@@ -19,8 +16,7 @@ end
 
    # The method `printer` should output first the results of the batch_badge_creator method and then of the assign_rooms method to the screen - this way you can output
     # the badges and room assignments one at a time.
-    # To make this test pass, make sure you are iterating through your badges and room assignments lists.
-
+    # To make this test pass, make sure you are iterating through your badges and room assignments lists
 def printer(names)
     batch_badge_creator(names).each {|name| puts name}
     assign_rooms(names).each {|name| puts name}
